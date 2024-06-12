869c11fee0114d5353d4708c9665dba69a197d15
jahdkaran <77935130+jahdkaran@users.noreply.github.com>
2024-06-12 13:08:56 +0330

    Initial commit7

--- l2cs/model.py
+++ l2cs/model.py
@@ -74,7 +74,7 @@ class MobileNetV3Gaze(nn.Module):
     def __init__(self, num_bins):
         super(MobileNetV3Gaze, self).__init__()
         # Create a MobileNetV3 Small model
-        self.mobilenet = torchvision.models.mobilenet_v3_small(pretrained=True)
+        self.mobilenet = torchvision.models.mobilenet_v3_small(Weights=None)
         self.mobilenet.classifier = nn.Identity()  # Remove the original classifier
 
         # Define new classifiers for yaw and pitch
